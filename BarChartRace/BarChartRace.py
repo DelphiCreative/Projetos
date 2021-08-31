543687a758e7dfbf680f4956d0647cdadd50fc9b
DiegoCataneo <diegohenriquecataneo@gmail.com>
2021-08-31 14:06:31 -0300

    Update BarChartRace.py
    Atualização no código

--- BarChartRace/BarChartRace.py
+++ BarChartRace/BarChartRace.py
@@ -1,41 +1,24 @@
-import sqlite3
+import sqlite3 
 import pandas as pd
 import bar_chart_race as bcr
 
 db = sqlite3.connect('Vendas.sdb')
-'''
-tabela = db.cursor()
 
-tabela.execute('SELECT ID,PRODUTO, strftime("%Y/%m", Data_Venda) Periodo,  Count(*) Total_Periodo FROM VENDAS '+
-               'GROUP BY strftime("%Y/%m", Data_Venda) , PRODUTO LIMIT 10')
+tabela = pd.read_sql('SELECT strftime("%Y/%m", DATA_VENDA) PERIODO, PRODUTO, COUNT(*) TOTAL_PERIODO FROM VENDAS '+
+                     ' WHERE strftime("%Y%m", DATA_VENDA) < "202201"'                  
+                     ' GROUP BY strftime("%Y/%m", DATA_VENDA), PRODUTO' ,con= db )
 
-print(tabela.fetchall())
-print('')
-'''
-
-tabela = pd.read_sql('SELECT ID,PRODUTO, strftime("%Y/%m", Data_Venda) Periodo,  Count(*) Total_Periodo FROM VENDAS '+
-                     ' WHERE strftime("%Y", Data_Venda) < "2022" '+   
-                     'GROUP BY strftime("%Y/%m", Data_Venda) , PRODUTO ',con = db)
-
-tabela = tabela.pivot_table(values = 'Total_Periodo',
-                            index =['Periodo'],
-                            columns = 'PRODUTO'
-                            )
+tabela = tabela.pivot_table(values='TOTAL_PERIODO',
+                            index= 'PERIODO',
+                            columns='PRODUTO'  )
 
 tabela.fillna(0,inplace=True)
-print(tabela)
 
-tabela = tabela.iloc[:,:].cumsum()
+tabela = tabela.cumsum()
 
-print(tabela)
-
-bcr.bar_chart_race(df = tabela,
-                  n_bars = 6,
-                  sort = 'desc',
-                  title = 'Gráfico de corrida de barras',
-                  filename = 'vendas_youtube.mp4'
-
-                  )
-
-
-
+bcr.bar_chart_race(df=tabela, 
+                   n_bars=6,
+                   title='GRÁFICO DE CORRIDA DE BARRAS',
+                   sort= 'desc',
+                   filename='vendas_canal.mp4'       
+)
